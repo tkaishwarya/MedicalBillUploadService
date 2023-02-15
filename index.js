09409de7344737810803f14fdf8c2352bb2043b9
TKAishwarya1601 <43754775+tkaishwarya@users.noreply.github.com>
2023-02-14 20:39:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const bills = [];
+
+app.get('/items', (req, res) => {
+  res.json(bills);
+});
+
+app.post('/items', (req, res) => {
+  const bill = req.body;
+  bills.push(bill);
+  res.json(bill);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
